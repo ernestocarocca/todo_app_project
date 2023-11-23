c695c651c3246a16265cd75ce8c74835bb83bd20
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2023-11-23 16:22:19 +0100

    create test button class,sharepref set and getlist

--- lib/pages/addtodo_page.dart
+++ lib/pages/addtodo_page.dart
@@ -1,12 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+
 import 'package:flutter/material.dart';
+
 import 'package:todo_app_project/mobile_storage/shared_pref.dart';
 
 class AddTodoPage extends StatelessWidget {
+   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Addtodos'),
+        title: const Text('Addtodos'),
       ),
       body: Column(
         children: [
@@ -17,49 +22,82 @@ class AddTodoPage extends StatelessWidget {
   }
 }
 
-class TaskList extends StatelessWidget {
-  TaskList({super.key});
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<String> getTodoList = [];
+  late final String label;
+  String name = '';
+  @override
+  void initState() {
+    super.initState();
+
+      getTodoList = SharedPreferencesManager.getTodos() ?? [];
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        TaskItem(label: "1"),
-     
-      ],
+      children: [buttonGetFromSherePref(getTodoList),
+                createTextList(getTodoList)],
     );
   }
 }
+// Ernesto: add todos in a list
+Widget buttonGetFromSherePref(List<String>getTodoList) => ButtonWidget(
+text: 'save',
+  onClicked: () async{ 
+    await SharedPreferencesManager.setTodos(getTodoList);
+    },
 
-class TaskItem extends StatelessWidget {
-  final List<String>? getNameList = SharedPreferencesManager.getTodos();
-  final String label;
 
-  TaskItem({
+);
+
+
+// Ernesto: Forlop that gets each string in a list
+Widget createTextList(List<String> getNameList) {
+  //List<String> textWidgets = [];
+  String names = '';
+  for (String todos in getNameList) {
+    names = todos;
+  }
+
+  return ListTile(title: Text(names));
+}
+
+
+//Ernesto button class that is the bluprint for buttonGetFromSherePref
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
     Key? key,
-    required this.label,
+    required this.onClicked,
+    required this.text,
+    
   }) : super(key: key);
 
-  void addString() async {
-    Future add = SharedPreferencesManager.setTodos(getNameList!);
-
-    print(add);
-  }
-
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-      
-        Text(label),
-        FloatingActionButton(onPressed: addString),
-      ],
-    );
-  }
+  Widget build(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size.fromHeight(52), backgroundColor: Colors.white,
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        onPressed: onClicked,
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      );
 }
-
-
-
 
 /*
 
